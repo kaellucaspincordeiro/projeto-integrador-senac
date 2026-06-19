66d1475836f7f192b5491c24a65b46949083d9bc
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-06-19 15:20:25 -0300

    Adicionando a função deletar_equipamento

--- equipamento/cadastro_equipamento.py
+++ equipamento/cadastro_equipamento.py
@@ -40,14 +40,66 @@ def montar_cadastro_equipamento(container, navegar):
 
         if cadastro_equipamento:
             messagebox.showinfo("Sucesso", "Equipamento cadastrado!")
-            navegar("dashboard")
         else:
             messagebox.showerror("Erro", "Ja existe um equipamento cadastrado.")
+
+    def deletar_equipamento():
+        item_selecionado = tabela.selection()
+
+        valores = tabela.item(item_selecionado, "values")
+        id_equipamento = valores[0]
+
+        if messagebox.askyesno("Confirmar", "Deseja excluir este equipamento?"):
+            bd.deletar_equipamento(id_equipamento)
 
     #Esperando o command para editar e excluir o registro do equipamento
     tk.Button(frame_cadastro_equipamento, text="Salvar", command=salvar_equipamento, bg="#1f4fc4", fg="white").grid(row=6, column=0, pady=(0, 15))
     tk.Button(frame_cadastro_equipamento, text="Editar", bg="#1f4fc4", fg="white").grid(row=7, column=0, pady=(0, 15))
-    tk.Button(frame_cadastro_equipamento, text="Excluir", bg="#d32d2d", fg="black").grid(row=8, column=0, pady=(0, 15))
+    tk.Button(frame_cadastro_equipamento, text="Excluir", command=deletar_equipamento ,bg="#d32d2d", fg="black").grid(row=8, column=0, pady=(0, 15))
+
+    frame_tabela_equipamento = tk.Frame(container)
+    frame_tabela_equipamento.grid(row=10, column=0, pady=15, padx=20, sticky="nsew")
+
+    colunas = ("id", "equipamento", "quantidade")
+
+    tabela = ttk.Treeview(
+        frame_tabela_equipamento,
+        columns=colunas,
+        show="headings",
+        height=7
+    )
+
+    # Cabeçalhos
+    tabela.heading("id", text="Id")
+    tabela.heading("equipamento", text="Nome do Equipamento")
+    tabela.heading("quantidade", text="Quantidade Disponível")
+
+    # Tamanho das colunas
+    tabela.column("id", width=50, anchor="center")
+    tabela.column("equipamento", width=100, anchor="center")
+    tabela.column("quantidade", width=20, anchor="center")
+
+    # Scrollbar vertical
+    scroll_y = ttk.Scrollbar(
+        frame_tabela_equipamento,
+        orient="vertical",
+        command=tabela.yview
+    )
+
+    tabela.configure(yscrollcommand=scroll_y.set)
+
+    tabela.grid(row=0, column=0, sticky="nsew")
+    scroll_y.grid(row=0, column=1, sticky="ns")
+
+    # Permite a tabela expandir dentro do frame
+    frame_tabela_equipamento.grid_rowconfigure(0, weight=1)
+    frame_tabela_equipamento.grid_columnconfigure(0, weight=1)
+
+    # --- CARREGAR DADOS NA TABELA ---
+    equipamentos = bd.listar_equipamentos()
+
+    for linha in equipamentos:
+        tabela.insert("", "end", values=linha)
 
 
 
